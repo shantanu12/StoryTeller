c2e421da8be84f2a1e57bdb944967c4f3bdf8832
shantanu12 <shantanu88@tamu.edu>
2016-04-04 17:17:04 -0500

    Fixed the name on the last page

--- app/src/main/java/edu/tamu/storyteller/FinalStarsFragment.java
+++ app/src/main/java/edu/tamu/storyteller/FinalStarsFragment.java
@@ -1,11 +1,14 @@
 package edu.tamu.storyteller;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 /**
  * Created by Shantanu on 4/3/2016.
@@ -14,6 +17,10 @@ public class FinalStarsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_final_stars, container, false);
         ImageView stars = (ImageView) v.findViewById(R.id.stars_imageView);
+        TextView text = (TextView) v.findViewById(R.id.textView);
+        SharedPreferences appPrefs = getActivity().getSharedPreferences(getString(R.string.app_prefs), Context.MODE_PRIVATE);
+        String userName = appPrefs.getString(getString(R.string.user_name), "");
+        text.setText("Congratulations " + userName + "!");
         Bundle data = getArguments();
         int score = data.getInt("score");
         switch (score) {
